d6057cfff2c956eb752577820a477d43a40564c3
AxelDeRosa <axelnderosa@gmail.com>
2022-11-12 20:43:24 -0300

    Initial commit

---
+++ Assets/scripts/objetoInteractuable.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objetoInteractuable : MonoBehaviour
+{
+    public bool agarrado = false;
+    public Sprite popout;
+    private SpriteRenderer spriteRender;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRender = GetComponent<SpriteRenderer>();
+    }
+    private void OnMouseOver()
+    {
+        if (!agarrado)
+        {
+            spriteRender.color = new Color(1, 0.8160377f, 0.8160377f, 1);
+
+        }
+        //Debug.Log("Mouse over");
+    }
+    private void OnMouseExit()
+    {
+        if (!agarrado)
+        {
+            spriteRender.color = new Color(1, 1, 1, 1);
+
+        }
+    }
+    private void OnMouseDown()
+    {
+        agarrado = true;
+        spriteRender.sprite = popout;
+    }
+    private void OnMouseEnter()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Bahia-Game-Jam-2022
+ Videojuego para la Bahia game jam tematica: Teatro Municipal
